e416568e0d38c893ffcb62b2774374fe2a77c475
Ian Jackson <ian.jackson@citrix.com>
2019-02-22 12:24:35 +0000

    pygrub: Specify -rpath LIBEXEC_LIB when building fsimage.so
    If LIBEXEC_LIB is not on the default linker search path, the python
    fsimage.so module fails to find libfsimage.so.
    
    Add the relevant directory to the rpath explicitly.
    
    (This situation occurs in the Debian package, where
    --with-libexec-libdir is used to put each Xen version's libraries and
    utilities in their own directory, to allow them to be coinstalled.)
    
    Signed-off-by: Ian Jackson <ian.jackson@citrix.com>
    
    Gbp-Pq: Name 0018-pygrub-Specify-rpath-LIBEXEC_LIB-when-building-fsima.patch

--- tools/pygrub/setup.py
+++ tools/pygrub/setup.py
@@ -8,10 +8,15 @@ import sys
 
 extra_compile_args  = [ "-fno-strict-aliasing" ]
 
+extra_link_args = []
+try: extra_link_args += [ "-Wl,-rpath," + os.environ['LIBEXEC_LIB'] ]
+except KeyError: pass
+
 XEN_ROOT = "../.."
 
 xenfsimage = Extension("xenfsimage",
     extra_compile_args = extra_compile_args,
+    extra_link_args = extra_link_args,
     include_dirs = [ XEN_ROOT + "/tools/libfsimage/common/" ],
     library_dirs = [ XEN_ROOT + "/tools/libfsimage/common/" ],
     libraries = ["xenfsimage"],
